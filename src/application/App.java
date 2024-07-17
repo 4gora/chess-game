aa053a9e444b70688bd22f6ce9b76c7d1b697161
Alessandro Rosario <rosarioalessandro03@gmail.com>
2024-07-17 11:01:42 -0300

    Adicionado ChessException para lidar com excecoes do jogo; ChessPosition criada para traduzir a entrada do usuário para matriz no tabuleiro; Pequenas modificacoes nos demais arquivos

--- src/application/App.java
+++ src/application/App.java
@@ -4,6 +4,8 @@ import chess.ChessMatch;
 
 public class App {
     public static void main(String[] args) {
+
+        System.out.println();
     
         ChessMatch chessMatch = new ChessMatch();
 
