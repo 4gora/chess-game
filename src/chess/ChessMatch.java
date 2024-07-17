d797fb10f68e8b489f7df91fb1a0ff294ff467a4
Alessandro Rosario <rosarioalessandro03@gmail.com>
2024-07-17 19:31:55 -0300

    Adicionado movimentos possiveis da peca

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -45,6 +45,9 @@ public class ChessMatch {
         if (!board.thereIsAPiece(position)) {
             throw new ChessException("Não existe peça nessa posição.");
         }
+        if (!board.piece(position).isThereAnyPossibleMove()) {
+            throw new ChessException("Não há movimentos possíveis para a peça escolhida.");
+        }
     }
 
     private void placeNewPiece(char column, int row, ChessPiece piece) {
