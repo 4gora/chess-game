88c55539e077e32dc945c394ad2436b86d92df89
Alessandro Rosario <rosarioalessandro03@gmail.com>
2024-07-17 11:59:54 -0300

    melhorias na impressao do tabuleiro

--- src/application/App.java
+++ src/application/App.java
@@ -5,10 +5,18 @@ import chess.ChessMatch;
 public class App {
     public static void main(String[] args) {
 
+        // Código para roda App 
+        // ↧ ↧ ↧
+        ///home/agr/Documentos/chess-game/bin
+        // ↧ ↧ ↧
+        // java --enable-preview -cp . application.App
+
         System.out.println();
     
         ChessMatch chessMatch = new ChessMatch();
 
         UI.printBoard(chessMatch.getPieces());
+
+        System.out.println();
     }
 }
